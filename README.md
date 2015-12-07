d1b27cd62e81f8a095ae1d96a5ef492838766392
ryoung3 <youngronique09@gmail.com>
2015-12-06 22:56:28 -0500

    Added files for neighborhood maps project
    Added files for neighborhood maps project

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# ryoung3.github.io-NeatAtlantaEats-
-Neat Atlanta Eats! locates restaurants,bars, &amp; diners within the Atlanta area that match your search criteria. 
+## Find ___ Near ___
+
+- Udacity Frontend Nanodegree Project 5, Neighborhood Map
+  By Justin Murdock
+
+TO RUN:
+
+View the demo [here](http://juliusakula.github.io/project-5), or clone the repo onto your computer and open index.html.
+Think of something to search for and type it in "Keyword" input field at the top of the page (in the navigation bar), and enter the address or city in the "Near:" box.
+
+Click search. A menu will open on the right with your search results.
+
+Put your mouse over the thumbnail and the location will bounce on the map.
+
+Click on the marker to see the name and address of the marker.
---
+++ js/main.js
@@ -0,0 +1,192 @@
+/*    Neighborhood Map
+
+Insert a search term and an address, and it will return a list of search items
+puts them on the map and on the side menu.
+
+This uses NPM, Knockout.js, jquery, bootstrap, Google Maps API and Yelp API.  */
+
+//set whole script to be strict
+'use strict';
+
+//sets cache for true so Timestamp parameter isn't added to ajax query.
+$.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
+    if ( options.dataType == 'jsonp' || originalOptions.dataType == 'jsonp' ) {
+        options.cache = true;
+    }
+});
+
+//init globals
+var map;
+var markers = [];
+
+//make Markers functions - clear, show, add, make
+
+var clearMarkers = function() {
+    setAllMap( null );
+    markers = [];
+};
+var showMarkers = function() {
+    setAllMap( map );
+};
+var setAllMap = function( map ) {
+    var len = markers.length;
+    for (var i = 0; i < len; i++){
+        markers[i].setMap( map );
+    }
+};
+var addMarker = function( marker ) {
+    markers.push( marker );
+};
+var makeMarker = function( coords, info ) {
+    var myLatLng = new google.maps.LatLng( coords.latitude, coords.longitude );
+    var infowindow = new google.maps.InfoWindow({
+        content: info
+    });
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        animation: google.maps.Animation.DROP
+    });
+    google.maps.event.addListener(marker, 'mouseover', function() {
+        infowindow.open(map, marker);
+    });
+    google.maps.event.addListener(marker, 'mouseout', function() {
+        infowindow.close(map, marker);
+    });
+    return marker;
+};
+var bounce = function( marker ) {
+    marker.setAnimation( google.maps.Animation.BOUNCE );
+};
+var stopBounce = function( marker ) {
+    marker.setAnimation( null );
+};
+
+
+//Listing model
+var Listing = function( data ) {
+    this.display_phone = ko.observable( data.display_phone );
+    this.image_url = ko.observable( data.image_url );
+    this.location = ko.observable( data.location.address[0] );
+    this.name = ko.observable( data.name );
+    this.rating = ko.observable( data.rating );
+    this.rating_url = ko.observable( data.rating_img_url );
+    this.url = ko.observable( data.url );
+    this.snippet_text = ko.observable( data.snippet_text );
+    this.review_count = ko.observable( data.review_count );
+    this.marker = makeMarker( data.location.coordinate, data.name + "<br>" + data.location.address[0] );
+    addMarker( this.marker );
+    //animates marker when mouseover Div
+    this.show = function() {
+        bounce(this.marker);
+    };
+    this.hide = function() {
+        stopBounce(this.marker);
+    };
+};
+
+var ViewModel = function() {
+    var self = this;
+    this.listings = ko.observableArray([]);
+    var geocoder;
+    var latlng;
+
+    //loads map
+    var initialize = function() {
+        var lat = 33.7550;
+        var lng = -84.3900;
+        //if geolocation is avail, set map to current position
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(function(position){
+                lat = position.coords.latitude;
+                lng = position.coords.longitude;
+            }, function(error){
+                console.log("Error: " + error.message);
+            });
+        }
+        latlng = new google.maps.LatLng( lat, lng );//sets to Atlanta, Georgia by default
+        var mapOptions = {
+          zoom: 12,
+          center: latlng
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    };
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+    //search handler function
+    this.search = function() {
+        var counter = 0;
+        var address = "Atlanta, Georgia";
+        var terms = $("#foodSearch").val();
+        moveMap(address);
+        yelp(terms,address);
+    };
+
+    //moves map to location
+    var moveMap = function(address) {
+        geocoder = new google.maps.Geocoder();
+        geocoder.geocode( { 'address': address}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+          } else {
+              console.log('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+    };
+
+    //queries yelp api with term and location, returns jsonp with results
+    var yelp = function(terms, location){
+        var auth = {
+            consumerKey : "Mn1Aj3R5cQKlwZveqnu8CQ",
+            consumerSecret : "DJWBTfFhvkUeznJE_q3RkLrLJnQ",
+            accessToken : "YLBx5vt_-2kf_2WXz6wiqM8qbL6I8R0C",
+            accessTokenSecret : "brBVqrs758RW5TDlRrs-7KlOZDQ",
+            serviceProvider : {
+                signatureMethod : "HMAC-SHA1"
+            }
+        };
+        var accessor = {
+            consumerSecret : auth.consumerSecret,
+            tokenSecret : auth.accessTokenSecret
+        };
+        var parameters = [];
+        parameters.push(['term', terms]);
+        parameters.push(['location', location]);
+        parameters.push(['callback', 'cb']);
+        parameters.push(['oauth_consumer_key', auth.consumerKey]);
+        parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+        parameters.push(['oauth_token', auth.accessToken]);
+        parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+        var message = {
+            'action' : 'http://api.yelp.com/v2/search',
+            'method' : 'GET',
+            'parameters' : parameters
+        };
+        OAuth.setTimestampAndNonce(message);
+        OAuth.SignatureMethod.sign(message, accessor);
+        var parameterMap = OAuth.getParameterMap(message.parameters);
+        $.ajax({
+            'url' : message.action,
+            'data' : parameterMap,
+            'dataType' : 'jsonp',
+            'cached' : true,
+            'jsonpCallback' : 'cb',
+            //empties listings array, then pushes in new results
+            'success' : function(data, textStats, XMLHttpRequest) {
+                clearMarkers();
+                self.listings.removeAll();
+                console.log(data);
+                var len = data.businesses.length;
+                for (var i = 0; i < len; i++){
+                    self.listings.push(new Listing(data.businesses[i]));
+                }
+                console.log(self.listings());
+                showMarkers();
+            }
+        }).fail(function(e) {
+            console.log('something went wrong: ');
+            console.log(e.error());
+        });
+    };
+};
+
+ko.applyBindings(new ViewModel());
